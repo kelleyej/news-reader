336c00162ac28fac5c63aca9557d9f5dd1e55a9f
Erin Kelley <erin.kelley01@gmail.com>
2024-05-02 09:46:27 -0600

    Add mock data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import NavBar from './NavBar';
 import Articles from './Articles';
+import { useState, useEffect } from 'react';
+import data from './mockdata';
 
 function App() {
+const [articles, setArticles] = useState([])
+
+useEffect(() => {
+  setArticles(data)
+}, [])
+console.log(articles)
+
   return (
     <div className="App">
       <NavBar />
--- src/mockdata.js
+++ src/mockdata.js
@@ -1,4 +1,4 @@
-data = [
+const data = [
     {
         headline: "Today's top story",
         image: "https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg",
