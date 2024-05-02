fd18efa2cdd6afbfa00adcb2f2b7b2445c6e23d6
Erin Kelley <erin.kelley01@gmail.com>
2024-05-02 11:57:58 -0600

    Add search bar

--- src/Articles.js
+++ src/Articles.js
@@ -1,22 +1,30 @@
 import './Articles.css';
 import ArticleCard from './ArticleCard';
+import { useState } from 'react';
 
 export default function Articles({ allArticles }) {
-    const articleGrid = allArticles.map(article => {
+const [search, setSearch] = useState('')
+const filteredArticles = allArticles.filter(article => article.title.toLowerCase().includes(search.toLowerCase()))
+   
+    const articleGrid = filteredArticles.map(article => {
        return (
             <ArticleCard
             image={article.urlToImage}
             title={article.title}
-           description={article.description}
+            description={article.description}
             publishedAt={article.publishedAt} 
            />
     ) 
     })
 
     return (
+        <div>
+        <input type='text' value={search} placeholder='Search title by keyword' onChange={(event) => setSearch(event.target.value)}/>
         <section className="article-container">
             {articleGrid}
-        </section>
+        </section> 
+        </div>
+       
     )
     
 }
