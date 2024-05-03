d89a0e647ef3d94bfcb1fc0e0839ba5370462994
Erin Kelley <erin.kelley01@gmail.com>
2024-05-03 17:40:26 -0600

    Add home button from not found page

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -6,7 +6,7 @@ export default function ArticleCard({ title, image, description, publishedAt }) 
         <Link to={`/articles/${title}`}>
             <section className='article-styling'>
                 <p>{(new Date(publishedAt)).toLocaleString()}</p>   
-                {(!image) ? <div className='no-image'><p>No image available.</p></div> : <img src={image} />}
+                {(!image) ? <div className='no-image'><p>No image available.</p></div> : <img className='img-style' src={image} />}
                 <h2>{title}</h2>
                 {(!description) ? <p>No description available for this article.</p> : <p className='description'>{description}</p>}
             </section>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import './NotFound.css';
+import { Link } from 'react-router-dom'; 
 
 export default function NotFound() {
     return (
         <section className='error-container'>
             <h4 className='error-message'>Oh no! We can't seem to find that page. Please try again later.</h4>
+            <Link to='/'><button className='home'>Home</button></Link>
         </section>
     )
 }
