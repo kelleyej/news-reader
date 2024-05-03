41e231510aacf0974fc24e36fdeec4c4cd27705c
Erin Kelley <erin.kelley01@gmail.com>
2024-05-03 15:26:22 -0600

    Add loading state

--- src/components/ArticleDetails/ArticleDetails.js
+++ src/components/ArticleDetails/ArticleDetails.js
@@ -1,9 +1,11 @@
 import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import './ArticleDetails.css';
+import NotFound from '../NotFound/NotFound';
 
 export default function ArticleDetails({ allArticles }) {
     const [articleDetails, setArticleDetails] = useState([])
+    const [loading, setLoading] = useState(true)
 
     const articleTitle = useParams().title
     useEffect(() => {
@@ -13,23 +15,30 @@ export default function ArticleDetails({ allArticles }) {
     function getArticleDetails() {
         const specificArticle = allArticles.filter(article => article.title === articleTitle)
         setArticleDetails(specificArticle)
+        setLoading(false)
     }
 
-    if(articleDetails.length === 0){
+    if(!articleDetails.length){
         return (
-            <p>Loading...</p> 
+            <NotFound />
         )
     } else {
+        
         return (
-        <div className='details-container'>
+        <div>
            <Link to="/"><button>⬅</button></Link>
-           <h2 className='title'>{articleDetails[0].title}</h2>
-           <p>{(new Date(articleDetails[0].publishedAt)).toLocaleString()}</p>
-            {(!articleDetails[0].urlToImage) ? <div className='no-image'><p>No image available.</p></div> : <img src={articleDetails[0].urlToImage} />}
-            <p className='content'>{articleDetails[0].content}</p>
-            <p className='source'>Source: {articleDetails[0].source.name}</p>
+           {(loading) ? <p>Loading...</p> : 
+           <div className='details-container'>
+             <h2 className='title'>{articleDetails[0].title}</h2>
+                <p>{(new Date(articleDetails[0].publishedAt)).toLocaleString()}</p>
+                {(!articleDetails[0].urlToImage) ? <div className='no-image'><p>No image available.</p></div> : <img src={articleDetails[0].urlToImage} />}
+                <p className='content'>{articleDetails[0].content}</p>
+                <p className='source'>Source: {articleDetails[0].source.name}</p>
+           </div>
+           }
+               
         </div> 
-    )
-    }
+    )}
+      
     
 }
