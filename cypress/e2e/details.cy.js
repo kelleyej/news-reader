5d2b2ecf74ca438a5bf17073f915393d8ec831cc
Erin Kelley <erin.kelley01@gmail.com>
2024-05-03 17:25:10 -0600

    Add api key to cypress config file

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -1,8 +1,6 @@
 describe('details page', () => {
     beforeEach(() => {
-     const apiKey = Cypress.env('REACT_APP_API_KEY')
-    
-     cy.intercept('GET', `https://newsapi.org/v2/top-headlines?country=us&apiKey=020fb8678e4a4f80a349b777eed308c6`, {
+     cy.intercept('GET', `https://newsapi.org/v2/top-headlines?country=us&apiKey=${Cypress.env('access_key')}`, {
        statusCode: 200, 
        fixture: 'articles'
      }), 
